d1f637d3055b685a8b08de5d096070807f4b2f0a
Nikolas Neto <nikoaccampora@gmail.com>
2021-09-01 20:50:48 -0300

    adding eclipe workspace content

---
+++ aula_02_hospital/src/aula_02_hospital/Internacao.java
@@ -0,0 +1,9 @@
+package aula_02_hospital;
+
+public class Internacao {
+	public String medicoResponsavel;
+	public Paciente paciente;
+	public String dataInternacao;
+	public String dataAlta;
+	public String observacoes;
+}
---
+++ aula_02_hospital/src/aula_02_hospital/Main.java
@@ -0,0 +1,15 @@
+package aula_02_hospital;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Criando um objeto
+		Endereco end1 = new Endereco(); 
+		Endereco end2 = null;
+		
+		// Imprimir o nome
+		System.out.print(end1.rua);
+		System.out.print(end2.rua);
+	}
+
+}
